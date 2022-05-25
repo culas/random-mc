ca3810fdfe5ad031e2af2e6b567703e9169e88c4
culas <lucas.schnueriger@me.com>
2022-05-25 14:47:31 +0200

    Add tailwind and some basic styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,10 @@
-<h1>Random Minecraft</h1>
+<h1 class="text-3xl font-bold">Random Minecraft</h1>
 <p>Helper to keep track of your randomized Minecraft playthrough.</p>
 
 <form method="post">
 	<label>
-		Name of Playthrough
-		<input type="text" name="playthrough"/>
+		<span>Name of Playthrough</span>
+		<input type="text" name="playthrough" />
 	</label>
 	<button type="submit">open</button>
 </form>
--- src/routes/playthrough/[code].svelte
+++ src/routes/playthrough/[code].svelte
@@ -5,14 +5,14 @@
 
 <h1>{code}</h1>
 
-<form method="post">
+<form method="post" class="mb-8">
 	<label>
-		block
-		<input type="text" name="block" />
+		<span>block</span>
+		<input type="text" name="block" placeholder="block" />
 	</label>
 	<label>
-		drop
-		<input type="text" name="drop" />
+		<span>drop</span>
+		<input type="text" name="drop" placeholder="dropped item" />
 	</label>
 	<button type="submit">add link</button>
 </form>
@@ -20,12 +20,14 @@
 <h2>Chains</h2>
 <main>
 	{#each chains as chain}
-		<section>
+		<section class="flex flex-wrap gap-2 mb-6">
 			{#if chain}
 				{#each chain as item}
-					<div>
-						{item}
-						<form method="post" action="?_method=DELETE">
+					<div class="p-2 pl-4 relative rounded shadow-lg hover:shadow-xl transition duration-150 ease-in-out bg-neutral-900 text-white whitespace-nowrap">
+						<div class="arrow-l absolute left-0 top-3 border-solid border-l-white border-l-8 border-y-transparent border-y-8 border-r-0"></div>
+						<div class="arrow-r absolute -right-2 top-3 border-solid border-l-neutral-900 border-l-8 border-y-transparent border-y-8 border-r-0"></div>
+						<span>{item}</span>
+						<form method="post" action="?_method=DELETE" class="inline-block">
 							<input type="text" hidden name="item" value={item} />
 							<button type="submit">X</button>
 						</form>
@@ -37,20 +39,13 @@
 </main>
 
 <style>
-	section {
-		display: flex;
-		flex-wrap: wrap;
-		max-width: 100%;
-		overflow: hidden;
-	}
-
-	div {
-		padding: 0.5rem;
-		white-space: nowrap;
-	}
-
-	div + div::before {
-		content: '>';
-		margin-right: 1rem;
+	section button {
+		display: inline-block;
+		padding: 0;
+		width: 20px;
+		line-height: 20px;
+		font-size: x-small;
+		text-align: center;
+		border-radius: 100%;
 	}
 </style>
